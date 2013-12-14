e6a64e0cce364d259bdb5bd62e402cc3f4db2dfa
Ben Nizette <benjamin.nizette@anu.edu.au>
2013-12-14 13:12:16 +0800

    Update plotting agent

--- plot_hokuyo.py
+++ plot_hokuyo.py
@@ -3,14 +3,26 @@
 import pylab
 import hokuyo
 
+from math import sin, cos
+
 h = hokuyo.HokuyoURG('/dev/ttyACM0')
+
+fig=pylab.figure()
+pylab.ion()
+pylab.show()
+pylab.hold(False)
 
 while True:
 	try:
 		d = h.scan_once()
+		b = [h.index_to_radians(i) for i in range(len(d))]
 
-		pylab.plot(d)
-		pylab.show()
+		x = [ r * sin(bear) for r, bear in zip(d,b)]
+		y = [ r * cos(bear) for r, bear in zip(d,b)]
+
+		pylab.scatter(x,y)
+		pylab.axis([-5000,5000,-1000,5000])
+		pylab.draw()
 
 	except KeyboardInterrupt:
 		break
